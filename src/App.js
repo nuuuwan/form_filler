97bbe484512864e6282b3b35e36c447cb393b041
nuuuwan <nuuuwan@gmail.com>
2024-01-22 12:32:01 +0530

    lint only

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <h1>
-      Form Filler 
-    </h1>
-  );
+  return <h1>Form Filler</h1>;
 }
 
 export default App;
